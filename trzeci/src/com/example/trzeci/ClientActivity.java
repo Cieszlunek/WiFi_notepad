785afdebcca725d78931262c92d4c628cde1475e
Cieszlunek <jsamczuk@gmail.com>
2013-11-13 15:49:04 +0100

    13.11.2013

---
+++ trzeci/src/com/example/trzeci/ClientActivity.java
@@ -0,0 +1,139 @@
+package com.example.trzeci;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+
+public class ClientActivity extends Activity {
+	 
+  
+    private Button search;
+    
+    private Button create;
+    
+    private Button connect;
+ 
+    private String serverIpAddress;
+ 
+    private boolean connected = false;
+    
+    private String str;
+ 
+    private Handler handler = new Handler();
+    
+    private File mPath;
+    
+    private FileDialog fileDialog;
+    
+    private NewFileDialog newFileDialog;
+    
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client);
+
+        mPath = new File(Environment.getExternalStorageDirectory() + "//DIR//");
+        fileDialog = new FileDialog(this, mPath);
+        fileDialog.setFileEndsWith(".txt");
+        fileDialog.addFileListener(new FileDialog.FileSelectedListener() {
+            public void fileSelected(File file) {
+                Log.d(getClass().getName(), "selected file " + file.toString());
+            }
+        });
+        newFileDialog = new NewFileDialog(this);
+        
+        search = (Button) findViewById(R.id.search_button);
+        search.setOnClickListener(searchListener);
+        create = (Button) findViewById(R.id.create_button);
+        create.setOnClickListener(createListener);
+        connect = (Button) findViewById(R.id.connect_button);
+        connect.setOnClickListener(connectListener);
+    }
+    private OnClickListener searchListener = new OnClickListener() {
+    	@Override
+    	public void onClick(View v){
+    		fileDialog.createFileDialog();
+    	}
+    };
+    private OnClickListener connectListener = new OnClickListener() {
+ 
+        @Override
+        public void onClick(View v) {
+            if (!connected) {
+                serverIpAddress = "127.0.0.1";
+                if (!serverIpAddress.equals("")) {
+                	
+                    Thread cThread = new Thread(new ClientThread());
+                    cThread.start();
+                }
+                
+            }
+            
+        }
+    };
+    
+    private OnClickListener createListener = new OnClickListener(){
+    	@Override
+    	public void onClick(View v){
+    		newFileDialog.createNewFileDialog();
+    	}
+    }
+    
+    
+    ;
+ 
+    public class ClientThread implements Runnable {
+ 
+        public void run() {
+        	
+            try {
+                InetAddress serverAddr = InetAddress.getByName(serverIpAddress);
+                Log.d("ClientActivity", "C: Connecting...");
+                Socket socket = new Socket(serverAddr, 8080);
+                connected = true;
+                while (connected) {
+                    try {
+                        Log.d("ClientActivity", "C: Sending command.");
+                        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                            // where you issue the commands
+                        
+                            out.println("Hey Server!");
+                            Log.d("ClientActivity", "C: Sent.");
+                    } catch (Exception e) {
+                        Log.e("ClientActivity", "S: Error", e);
+                    }
+                }
+                socket.close();
+                Log.d("ClientActivity", "C: Closed.");
+            } catch (Exception e) {
+            	//textViewClient.post(new Runnable(){public void run(){textViewClient.setText("Cant connect to server!");}});
+                Log.e("ClientActivity", "C: Error", e);
+                connected = false;
+            }
+            
+            
+        }
+    }
+    
+
+    
+}
---
+++ trzeci/src/com/example/trzeci/NewFileDialog.java
@@ -0,0 +1,40 @@
+package com.example.trzeci;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.text.InputType;
+import android.widget.EditText;
+
+public class NewFileDialog {
+	
+	private final Activity activity;
+	public NewFileDialog(Activity activity){
+		this.activity = activity;
+	}
+	
+	public Dialog createNewFileDialog(){
+		Dialog dialog = null;
+		AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+		builder.setTitle("Enter file name");
+		final EditText input = new EditText(activity);
+		input.setInputType(InputType.TYPE_CLASS_TEXT);
+		builder.setView(input);
+		builder.setPositiveButton("OK", new DialogInterface.OnClickListener(){
+			@Override
+			public void onClick(DialogInterface dialog, int which){
+				String t = input.getText().toString();
+				//create file
+			}
+		});
+		builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+			@Override
+			public void onClick(DialogInterface dialog, int which){
+				dialog.cancel();
+			}
+		});
+		dialog = builder.show();
+		return dialog;
+	}
+}
